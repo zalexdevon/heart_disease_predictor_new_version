b1304aac98f1f6f117bf2e05850792df71a83023
Tâm Trần <tranminhtamtq2004@gmail.com>
2025-03-04 20:17:00 +0700

    c

--- src/classifier/components/data_transformation.py
+++ src/classifier/components/data_transformation.py
@@ -58,10 +58,13 @@ class AfterFeatureColumnTransformer(BaseEstimator, TransformerMixin):
         super().__init__()
 
     def fit(self, X, y=None):
+        self.pca = PCA(n_components=22)
+        self.pca.fit(X)
 
         return self
 
     def transform(self, X, y=None) -> pd.DataFrame:
+        X = pd.DataFrame(self.pca.transform(X))
 
         self.cols = X.columns.tolist()
         return X
